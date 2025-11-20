cc11d9facfed1821e51c6d29f015d71dd21cf492
Wesley Jones <wesjones2004@gmail.com>
2025-11-20 16:18:54 -0500

    remove comments

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -1,25 +1,6 @@
 "use client";
 
 export const LoginButton = () => {
-  /*
-
-fn run_google_sso(ctx: &egui::Context) {
-    const CLIENT_ID: &str =
-        "784057978591-sfb8ve6ba6k0u4jqkub4indov3m8lktv.apps.googleusercontent.com";
-    const SCOPE: &str = "email";
-
-    let google_auth_url = OpenUrl::same_tab(format!(
-        "https://accounts.google.com/o/oauth2/v2/auth?client_id={}&redirect_uri={}&response_type=code&scope={}",
-        CLIENT_ID,
-        format!("{}/auth/google/callback", BACKEND_PREFIX),
-        SCOPE
-    ));
-
-    ctx.open_url(google_auth_url);
-}
-
-*/
-
   const login = () => {
     const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "";
     const scope = "email";
