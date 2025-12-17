c5dfc34e371e5ce05b2dc8fecdc3dbcb4d487fb4
Wesley Jones <wesjones2004@gmail.com>
2025-12-17 13:11:41 -0500

    Enforce required fields in Project type and remove unused project entry

--- constants/projects.ts
+++ constants/projects.ts
@@ -3,9 +3,9 @@ export type Project = {
   title: string;
   summary: string;
   date: string;
-  image?: string;
-  tags?: string[];
-  link?: string;
+  image: string;
+  tags: string[];
+  link: string;
 };
 
 export const PROJECTS_LIST: Project[] = [
@@ -23,21 +23,5 @@ export const PROJECTS_LIST: Project[] = [
       "Performance",
     ],
     link: "https://github.com/nitrate-lang/nitrate",
-  },
-  {
-    id: "p2",
-    title: "Crypto Snail",
-    summary: "Public-domain cryptography library for educational purposes.",
-    date: "2024-01-27",
-    image: "/assets/placeholder-1.jpg",
-    tags: [
-      "C",
-      "Assembly",
-      "Cryptography",
-      "Performance",
-      "Optimization",
-      "Algorithms",
-    ],
-    link: "https://github.com/Kracken256/crypto-snail",
   },
 ];
