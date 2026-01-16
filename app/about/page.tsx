e7571b3874a6c80a9416c223ccf1cbae9a431b2f
Wesley Jones <wesjones2004@gmail.com>
2026-01-16 12:52:49 -0500

    Refactor About and Contact components for improved layout and styling consistency

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,16 +1,10 @@
 import React from "react";
-import SuperButton from "../../components/SuperButton";
 
 export default function About() {
   return (
-    <main className="min-h-screen flex items-center justify-center p-8">
-      <div className="relative w-full max-w-5xl">
-        <div
-          aria-hidden
-          className="absolute -inset-8 rounded-3xl blur-3xl transform-gpu"
-        >
-          <div className="absolute inset-0 rounded-3xl hidden dark:block bg-gradient-to-r from-indigo-400/30 via-sky-300/30 to-emerald-300/30" />
-        </div>
+    <main className="min-h-screen flex flex-col p-4 items-center bg-gradient-to-r from-indigo-400/30 via-sky-300/30 to-emerald-300/30">
+      <div className="flex flex-col w-full max-w-140 items-center gap-12">
+        a
       </div>
     </main>
   );
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,15 +2,14 @@ import React from "react";
 
 export default function Contact() {
   return (
-    <main className="min-h-screen flex justify-center p-8">
-      <div className="max-w-3xl w-full">
+    <main className="min-h-screen flex flex-col p-4 items-center bg-gradient-to-r from-indigo-400/30 via-sky-300/30 to-emerald-300/30">
+      <div className="flex flex-col w-full max-w-140 items-center gap-12">
         <h1 className="text-4xl font-bold mb-6">Contact Info</h1>
         <table className="w-full border-collapse border border-gray-400">
           <thead>
             <tr>
               <th className="border border-gray-400 px-4 py-2">Name</th>
               <th className="border border-gray-400 px-4 py-2">Email</th>
-              <th className="border border-gray-400 px-4 py-2">Phone Number</th>
             </tr>
           </thead>
           <tbody>
@@ -18,9 +17,6 @@ export default function Contact() {
               <td className="border border-gray-400 px-4 py-2">Wesley Jones</td>
               <td className="border border-gray-400 px-4 py-2">
                 <a href="mailto:ceo@atahgroup.com">ceo@atahgroup.com</a>
-              </td>
-              <td className="border border-gray-400 px-4 py-2">
-                <a href="tel:+15599086563">+1 (559) 908-6563</a>
               </td>
             </tr>
           </tbody>
